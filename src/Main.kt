eef76e98e21b2bf1055e678898c60c3424363be4
Jbustos17 <juandavidbustos04@gmail.com>
2025-02-14 13:43:08 -0500

    Salidas por consola

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,14 @@ fun main() {
  println("Hola Mundo")
     println("Primer Ejercicio Kotlin")
 
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+
+
+
+
 
 
 }
